aedb0fca3685c855d758eced49a6bce3ee326047
Jorge Miguel <81872020+J0rgeF@users.noreply.github.com>
2022-01-21 17:37:35 +0000

    Update main.c

--- main.c
+++ main.c
@@ -19,30 +19,39 @@ int grid(int currentposition)
 		|| r == 39 || r == 40
 		|| r == 49 || r == 50
 		|| r == 59 || r == 60
-		|| r == 69 || r == 70)
+		|| r == 69 || r == 70
+		|| r == 80 || r == 90
+		|| r == 100 || r == 110
+		|| r == 120 || r == 130
+		|| r == 140 || r == 150)
 	{
 		eaten = 1;
 	}
+
+
 	srand(time(NULL));
-	if(eaten == 1){
-		r = rand() % 78;
+	if (eaten == 1) {
+		r = rand() % 158;
+		r = rand() % 158;
 		eaten = 0;
 		nu = rand() % ((9 + 1) - 2) + 2; // Get a random number between 2 and 9
 	}
 	xy[r] = nu;
-	printf("Game Test in C\n");
+	printf("Move Test in C\n");
 	printf("Developed by Jorge\n");
-	printf("XXXXXXXXXX\n");
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[01], xy[11], xy[21], xy[31], xy[41], xy[51], xy[61], xy[71]);
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[02], xy[12], xy[22], xy[32], xy[42], xy[52], xy[62], xy[72]);
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[03], xy[13], xy[23], xy[33], xy[43], xy[53], xy[63], xy[73]);
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[04], xy[14], xy[24], xy[34], xy[44], xy[54], xy[64], xy[74]);
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[05], xy[15], xy[25], xy[35], xy[45], xy[55], xy[65], xy[75]);
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[06], xy[16], xy[26], xy[36], xy[46], xy[56], xy[66], xy[76]);
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[07], xy[17], xy[27], xy[37], xy[47], xy[57], xy[67], xy[77]);
-	printf("X%d%d%d%d%d%d%d%dX\n", xy[8], xy[18], xy[28], xy[38], xy[48], xy[58], xy[68], xy[78]);
-	printf("XXXXXXXXXX\n");
+	printf("XXXXXXXXXXXXXXXXXX\n");
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[01], xy[11], xy[21], xy[31], xy[41], xy[51], xy[61], xy[71],xy[81], xy[91], xy[101], xy[111], xy[121], xy[131], xy[141], xy[151]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[02], xy[12], xy[22], xy[32], xy[42], xy[52], xy[62], xy[72],xy[82], xy[92], xy[102], xy[112], xy[122], xy[132], xy[142], xy[152]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[03], xy[13], xy[23], xy[33], xy[43], xy[53], xy[63], xy[73],xy[83], xy[93], xy[103], xy[113], xy[123], xy[133], xy[143], xy[153]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[04], xy[14], xy[24], xy[34], xy[44], xy[54], xy[64], xy[74],xy[84], xy[94], xy[104], xy[114], xy[124], xy[134], xy[144], xy[154]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[05], xy[15], xy[25], xy[35], xy[45], xy[55], xy[65], xy[75],xy[85], xy[95], xy[105], xy[115], xy[125], xy[135], xy[145], xy[155]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[06], xy[16], xy[26], xy[36], xy[46], xy[56], xy[66], xy[76],xy[86], xy[96], xy[106], xy[116], xy[126], xy[136], xy[146], xy[156]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[07], xy[17], xy[27], xy[37], xy[47], xy[57], xy[67], xy[77],xy[87], xy[97], xy[107], xy[117], xy[127], xy[137], xy[147], xy[157]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[8], xy[18], xy[28], xy[38], xy[48], xy[58], xy[68], xy[78],xy[88], xy[98], xy[108], xy[118], xy[128], xy[138], xy[148], xy[158]);
+	printf("X%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%dX\n", xy[9], xy[19], xy[29], xy[39], xy[49], xy[59], xy[69], xy[79], xy[89], xy[99], xy[109], xy[119], xy[129], xy[139], xy[149], xy[159]);
+	printf("XXXXXXXXXXXXXXXXXX\n");
 	printf("You lose your points if you reach the limit of the map\n");
+	// printf("Position : %d\n", r);
 	printf("%d points\n", points);
 
 	if (currentposition == r) {
@@ -93,11 +102,15 @@ int play(int currentposition) {
 			printf("%c\n", c);
 			if (c == 's' || c == 'S') {
 				currentposition++;
-				if (currentposition != 9 && currentposition != 19
-					&& currentposition != 29 && currentposition != 39
-					&& currentposition != 49 && currentposition != 49
-					&& currentposition != 59 && currentposition != 69
-					&& currentposition != 79)
+				if (currentposition != 0 && currentposition != 10
+					&& currentposition != 20 && currentposition != 30
+					&& currentposition != 40 && currentposition != 50
+					&& currentposition != 60 && currentposition != 70
+					&& currentposition != 80 && currentposition != 90
+					&& currentposition != 100 && currentposition != 110
+					&& currentposition != 120 && currentposition != 130
+					&& currentposition != 140 && currentposition != 150
+					&& currentposition != 160)
 				{
 					system("cls");
 					grid(currentposition);
@@ -118,7 +131,11 @@ int play(int currentposition) {
 					&& currentposition != 20 && currentposition != 30
 					&& currentposition != 40 && currentposition != 50
 					&& currentposition != 60 && currentposition != 70
-					&& currentposition != 80)
+					&& currentposition != 80 && currentposition != 90
+					&& currentposition != 100 && currentposition != 110
+					&& currentposition != 120 && currentposition != 130
+					&& currentposition != 140 && currentposition != 150
+					&& currentposition != 160)
 				{
 					system("cls");
 					grid(currentposition);
@@ -136,7 +153,7 @@ int play(int currentposition) {
 			if (c == 'd' || c == 'D')
 			{
 				currentposition = currentposition + 10;
-				if (currentposition < 80)
+				if (currentposition <= 159)
 				{
 					system("cls");
 					//printf("%d\n", currentposition);
